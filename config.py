f3aa18a2b34d74609b011269162d040dfaf1cdbf
franmerzbow <bruhher228eblan@gmail.com>
2026-03-03 22:17:08 +0300

    added something

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 # config.py
+APP_NAME = "Advanced CLI Calculator"
+
+WELCOME_MESSAGE = "Добро пожаловать в продвинутый калькулятор!"
+
+ERROR_DIVISION_BY_ZERO = "Ошибка: деление на ноль!"
+ERROR_INVALID_OPERATION = "Ошибка: неизвестная операция!"
