719bd95fb09968ba458a9e7ebe627964da845e78
Dmitrii <sunmoth@mail.ru>
2026-03-03 21:20:19 +0300

    Added main function, waiting for the features

--- main.py
+++ main.py
@@ -15,3 +15,17 @@ init(autoreset=True)
 	prompt="Choose operation (+, -, *, /)",
 	type=click.Choise(["+", "-", "*", "/"]),
 )
+def main(a, b, operatoion):
+	click.echo(Fore.CYAN + config.APP_NAME)
+	click.echo(Fore.YELLOW + config.WELCOME_MESSAGE)
+
+	try:
+		result = utils.calculate(a, b, operation)
+		click.echo(Fore.GREEN + f"Result: {result}")
+	except ZeroDivisionError:
+		click.echo(Fore.RED + config.ERROR_DIVISION_BY_ZERO)
+	except ValueError:
+		click.echo(Fore.RED + config.ERROR_INVALID_OPERATION)
+
+if __name__ == "__main__":
+	main()
