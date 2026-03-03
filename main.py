b8e7fd4c7a8cd8ab21802e2ce1f3ce53ad9bc4c1
Dmitrii <sunmoth@mail.ru>
2026-03-03 21:15:34 +0300

    Added decorators for future def main

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ import click
 from colorama import init, Fore, Style
 import config
 import utils
+
+init(autoreset=True)
+
+@click.command()
+@click.optionn("--a", prompt="Input first number", type=float)
+@click.option("--b", prompt="Input second number", type=float)
+@click.option(
+	"--operation",
+	prompt="Choose operation (+, -, *, /)",
+	type=click.Choise(["+", "-", "*", "/"]),
+)
