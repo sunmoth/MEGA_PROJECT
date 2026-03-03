26e62514d773bb6898d5e279cff9246b18beeb28
franmerzbow <bruhher228eblan@gmail.com>
2026-03-03 22:19:59 +0300

    added other funcs than summin

--- utils.py
+++ utils.py
@@ -2,3 +2,15 @@
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError
+    return a / b
